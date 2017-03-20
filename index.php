094214c552e6ba8fa00df89443b1f128a2dc38f8
Umisagi <zenchiter@gmail.com>
2017-03-20 17:56:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -47,60 +47,34 @@ $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token
 
 //Initiate cURL.
 $ch = curl_init($url);
+//The JSON data.
+    $jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    },
+    "message":{
+        "text":"'.$message_to_reply.'"
+    }
+}';
+
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 
 
 
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
-        //The JSON data.
-        $jsonData = '{
-        "recipient":{
-            "id":"'.$sender.'"
-        },
-        "message":{
-            "text":"'.$message_to_reply.'"
-        }
-    }';
-
-    //Encode the array into JSON.
-    $jsonDataEncoded = $jsonData;
-
-    //Tell cURL that we want to send a POST request.
-    curl_setopt($ch, CURLOPT_POST, 1);
-
-    //Attach our encoded JSON string to the POST fields.
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-    //Set the content type to application/json
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-
-    $result = curl_exec($ch);
-}
-else{
-         //The JSON data.
-        $jsonData = '{
-        "recipient":{
-            "id":"'.$sender.'"
-        },
-        "message":{
-            "text":"Test"
-        }
-    }';
-
-    //Encode the array into JSON.
-    $jsonDataEncoded = $jsonData;
-
-    //Tell cURL that we want to send a POST request.
-    curl_setopt($ch, CURLOPT_POST, 1);
-
-    //Attach our encoded JSON string to the POST fields.
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-    //Set the content type to application/json
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-
+        
     $result = curl_exec($ch);
 }
 
