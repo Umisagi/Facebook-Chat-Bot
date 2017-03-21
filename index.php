958040a8bf9b35c9c96068bc8ccc018ef68552cd
Umisagi <zenchiter@gmail.com>
2017-03-21 14:07:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -56,8 +56,26 @@ $ch = curl_init($url);
         "id":"'.$sender.'"
     },
     "message":{
-        "text":"'.$message_to_reply.'"
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"What do you want to do next?",
+        "buttons":[
+          {
+            "type":"web_url",
+            "url":"https://petersapparel.parseapp.com",
+            "title":"Show Website"
+          },
+          {
+            "type":"postback",
+            "title":"Start Chatting",
+            "payload":"USER_DEFINED_PAYLOAD"
+          }
+        ]
+      }
     }
+  }
 }';
 
 //Encode the array into JSON.
