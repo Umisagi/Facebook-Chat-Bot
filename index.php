fcf38d7a9886522282a84462442d976f6f406210
Umisagi <zenchiter@gmail.com>
2017-03-23 16:14:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ error_log("####Messageid : ".$mid);
 $message_to_reply = '';
 
 // Search mid for tid
-$results = $facebook->api("/m_mid.$cAASz7lCTwNphKmOCzVa-R42xahk7")->get();
+$results = $facebook->api("/me")->get();
 if($results->error):
   return return_error('Notifications', $results->error);
 endif;
