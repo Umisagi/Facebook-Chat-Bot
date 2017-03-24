784e9a3d34a5d50f058356500603ffda5a1a2031
Umisagi <zenchiter@gmail.com>
2017-03-24 15:14:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ if(!empty($input['entry'][0]['messaging'][0]['message'])):
 
 elseif(!empty($input['entry'][0]['messaging'][0]['delivery'])):
     $mid = "m_".$input['entry'][0]['messaging'][0]['delivery']['mids'][0];
-    $results = $facebook->api("/{$mid}")->fields('from, to, created_time')->get();
+    $results = $facebook->api("/{$mid}")->fields('from, to, created_time, message')->get();
     if($results->error):
       return error_log('*************Error : '.print_r($results,true));
     endif;
