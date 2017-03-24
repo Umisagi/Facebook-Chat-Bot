c1a4fb973def5bd846ba89426799a43f2361dd89
Umisagi <zenchiter@gmail.com>
2017-03-24 09:18:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ $message = $input['entry'][0]['messaging'][0]['message']['text'];
 $time = $input['entry'][0]['messaging'][0]['timestamp']*0.001;
 $time = floor($time);
 $time = $time-1;
+$time2 = $input['entry'][0]['messaging'][0]['timestamp'];
 error_log("####Messageid : ".$mid);
 $message_to_reply = '';
 
@@ -49,7 +50,7 @@ if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $userid = $results->from->id;
     $pageid = $results->to->data[0]->id;
     error_log("----Time : ".$time);
-    
+    error_log("----Time2 : ".$time2);
     //Chk db
     $results = $facebook->api("/me/threads")->fields('participants')->since($time)->get();
     //error_log("-----Threads : ".print_r($results,true));
