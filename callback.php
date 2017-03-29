ee2ab49a5ce02d71d90c54501190230ca038ec60
Umisagi <zenchiter@gmail.com>
2017-03-29 17:40:20 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -21,18 +21,30 @@ if ($hub_verify_token === $verify_token) {
 }
 
 $input = json_decode(file_get_contents('php://input'), true);
-error_log("****INPUT : ".print_r($input,true));
+//error_log("****INPUT : ".print_r($input,true));
+$page_id = $input['entry'][0]['id'];
+$thread_id = 'Null';
+$message_id = "m_".$input['entry'][0]['messaging'][0]['message']['mid'];
 $message = $input['entry'][0]['messaging'][0]['message']['text']; // Incoming message
-$ctime = $input['entry'][0]['messaging'][0]['timestamp']; // Epoch timestamp
+$attachment = $input['entry'][0]['messaging'][0]['message']['attachments'];
+$results = $facebook->api("/{$message_id}")->fields('from, to, created_time')->get();
+if($results->error):
+    error_log('*************Error : '.print_r($results,true));
+endif;
+$sender_id = $results['from']['id'];
+$sebder_name = $results['from']['name]';
+$sender_email = $results['from']['email'];
+$receiver_id = $results['to']['data'][0]['id'];
+$receiver_name = $results['to']['data'][0]['name'];
+$receiver_email = $results['to']['data'][0]['email'];
+$created_time = $results['created_time']; // Epoch timestamp
 $time = $input['entry'][0]['messaging'][0]['timestamp']*0.001; // Ignore millisecond
 $time = floor($time);
 $time = $time-5;
 
-$message_to_reply = '';
-
 // Search mid for tid
 // Incoming message
-if(empty($input['entry'][0]['messaging'][0]['message']['is_echo'])):
+/*if(empty($input['entry'][0]['messaging'][0]['message']['is_echo'])):
     $mid = "m_".$input['entry'][0]['messaging'][0]['message']['mid']; // Message ID
     $results = $facebook->api("/{$mid}")->fields('from, to, created_time')->get();
     if($results->error):
@@ -95,18 +107,22 @@ elseif(!empty($input['entry'][0]['messaging'][0]['message']['is_echo'])):
         endwhile;
     endif;
     error_log("####Page Sending Mode####");
-endif;
+endif;*/
 
 //error_log("*****************************");
 //error_log("------Thread ID : ".$threadid);
 //error_log("------Create Time : ".$ctime);
 //error_log("------Update Time : ".$updatetime);
-//error_log("*****************************");
-//error_log("------Userid : ".$userid);
-//error_log("------Username : ".$username);
-//error_log("------Pageid : ".$pageid);
-//error_log("------Pagename : ".$pagename);
-//error_log("------Thread ID : ".$threadid);
-//error_log("------Message ID : ".$mid);
-//error_log("------Message : ".$message);
-//error_log("------Time : ".$createdtime);
+error_log("*****************************");
+error_log("------Page_id : ".$page_id);
+error_log("------Thread_id : ".$thread_id);
+error_log("------Message_id : ".$message_id);
+error_log("------Message : ".$message);
+error_log("------Attachment : ".$attachment);
+error_log("------Sender_id: ".$sender_id);
+error_log("------Sebder_name : ".$sebder_name);
+error_log("------Sender_email : ".$sender_email);
+error_log("------Receiver_id: ".$receiver_id);
+error_log("------Receiver_name : ".$receiver_name);
+error_log("------Receiver_email : ".$receiver_email);
+error_log("------Created_time : ".$created_time);
