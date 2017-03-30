36ca1d908aa3d035ed492505a640e9592882acc4
Umisagi <zenchiter@gmail.com>
2017-03-30 17:43:38 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -29,11 +29,11 @@ error_log("input : ".$input);
 $access_token = "EAAGHU7aBAlsBAKo1nqpDXS9DPIFgYaj6L05uEm2arLZBsFEvNpgYqg3dlxmYCbppRrNUl6QJNGu8GwghZC9LbWRsgXoZAyuaRwKuSV8ZAo5WtG1bsIvfbzTNEoX397AZAma3xDjBFv8ZCGAwCdUmB7fnStWepmJ6a5hTl4ntJLzwZDZD";
 $thread_id = '';
 //---------------if(count($query)>0)
-if(0)
+/*if(0)
 {
 	//$objects = $message_smm->getDataMessage($input, $access_token, $thread_id);
 } else
 {
 	//$objects = $message_smm->getThreadID($input, $access_token);
-}
+}*/
 //error_log("-----------objects : ".$objects);
