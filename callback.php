7d75bf1ac2871f40913c77fd51d57d2d1aa8a5c9
Umisagi <zenchiter@gmail.com>
2017-03-31 14:30:24 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ * Set default timezone to Bangkok
+ */
+date_default_timezone_set('Asia/Bangkok');
+ini_set("display_errors" , 1);
+ini_set("error_reporting" , E_ALL);
+/**
  * Include important files
  */
 //----------------require('CDatabase.php');
@@ -11,7 +17,6 @@ require('facebook-library.php');
  */
 //----------------$database = new CDatabase;
 //----------------$database->Connect();
-$message_smm = new CMessageFacebook;
 $verify_token = "just_do_it";
 $hub_verify_token = null;
 if(isset($_REQUEST['hub_challenge'])) 
@@ -27,18 +32,14 @@ $input = json_decode(file_get_contents('php://input'), true);
 //---------------$sqlString = "SELECT * FROM WEBHOOK_MESSAGE_THREAD WHERE (USER_ID = '".$input['entry'][0]['messaging'][0]['recipient']['id']."' OR USER_ID = '".$input['entry'][0]['messaging'][0]['sender']['id']."') AND PAGE_ID ='".$input['entry'][0]['id']."'";
 //---------------$query = $database->ExecuteReader2($sqlString, array());
 $access_token = "EAAGHU7aBAlsBAKo1nqpDXS9DPIFgYaj6L05uEm2arLZBsFEvNpgYqg3dlxmYCbppRrNUl6QJNGu8GwghZC9LbWRsgXoZAyuaRwKuSV8ZAo5WtG1bsIvfbzTNEoX397AZAma3xDjBFv8ZCGAwCdUmB7fnStWepmJ6a5hTl4ntJLzwZDZD";
-$thread_id = '';
+$message_smm = new CMessageFacebook($access_token);
 //---------------if(count($query)>0)
 if(0)
 {
-	$objects = $message_smm->getDataMessage($input, $access_token, $thread_id);
+	$thread_id = '';
+	$objects = $message_smm->getDataMessage($input, $thread_id);
 } else
 {
-	$objects = $message_smm->getThreadID($input, $access_token);
+	$objects = $message_smm->getThreadID($input);
 }
-error_log("objects : ".print_r($objects,true));
-
-
-
-
-
+error_log("objects : ".print_r($objects,true);
