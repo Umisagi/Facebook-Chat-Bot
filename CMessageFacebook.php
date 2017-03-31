a59a0f10993852fa5a29ecb6c39bbbb0a6e75868
Umisagi <zenchiter@gmail.com>
2017-03-31 14:02:04 +0700

    Update CMessageFacebook.php

--- CMessageFacebook.php
+++ CMessageFacebook.php
@@ -2,8 +2,7 @@
 class CMessageFacebook {
 
 	public function __construct($access_token = ''){
-		$this->access_token = $access_token;
-		$this->facebook = new Facebook($this->access_token);
+		$this->facebook = new Facebook($access_token);
 	}
 
 	public function getDataMessage( $input, $threadid){
@@ -14,7 +13,7 @@ class CMessageFacebook {
 		endif;
 		//data
 		$thread_id = $threadid;
-    		$page_id = $input['entry'][0]['id'];
+    	$page_id = $input['entry'][0]['id'];
 		$message_id = "m_".$input['entry'][0]['messaging'][0]['message']['mid'];
 		$message = $input['entry'][0]['messaging'][0]['message']['text']; // Incoming message
 		$attachment = $input['entry'][0]['messaging'][0]['message']['attachments'];
@@ -27,21 +26,21 @@ class CMessageFacebook {
 		$created_time = $results->created_time;
 		$data = (object)array(
 			"thread_id" => $thread_id,
-			"page_id" => $page_id,
-			"message_id" => $message_id,
-			"message" => $message,
-			"attachment" => $attachment,
-			"sender_id" => $sender_id,
-			"sebder_name" => $sebder_name,
-			"sender_email" => $sender_email,
-			"receiver_id" => $receiver_id,
-			"receiver_name" => $receiver_name,
-			"receiver_email" => $receiver_email,
-			"created_time" => $created_time,
-		);
-		  $callback[] = $data;
-		  return $callback;
-		}
+            "page_id" => $page_id,
+            "message_id" => $message_id,
+            "message" => $message,
+            "attachment" => $attachment,
+            "sender_id" => $sender_id,
+            "sebder_name" => $sebder_name,
+            "sender_email" => $sender_email,
+            "receiver_id" => $receiver_id,
+            "receiver_name" => $receiver_name,
+            "receiver_email" => $receiver_email,
+            "created_time" => $created_time,
+          );
+          $callback[] = $data;
+          return $callback;
+  	}
 
   	public function getThreadID($input){
   		$callback = [];
