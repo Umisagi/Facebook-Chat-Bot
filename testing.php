90cfb88c8802e905f9824697266bbc7d446bf7b3
Umisagi <zenchiter@gmail.com>
2017-04-05 13:32:17 +0700

    Update testing.php

--- testing.php
+++ testing.php
@@ -1,28 +1,13 @@
 <?php
 // Facebook class
-require('facebook-library.php');
-$access_token = "EAAGHU7aBAlsBAKo1nqpDXS9DPIFgYaj6L05uEm2arLZBsFEvNpgYqg3dlxmYCbppRrNUl6QJNGu8GwghZC9LbWRsgXoZAyuaRwKuSV8ZAo5WtG1bsIvfbzTNEoX397AZAma3xDjBFv8ZCGAwCdUmB7fnStWepmJ6a5hTl4ntJLzwZDZD";
-$facebook = new Facebook($access_token);
-$verify_token = "just_do_it";
-$hub_verify_token = null;
-if(isset($_REQUEST['hub_challenge'])) {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
-if ($hub_verify_token === $verify_token) {
-    echo $challenge;
-}
-$input = json_decode(file_get_contents('php://input'), true);
-//error_log("****INPUT : ".print_r($input,true));
-$sender = $input['entry'][0]['messaging'][0]['sender']['id']; // ID to send back 
-//API Url
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+
+$url = 'https://calm-retreat-75905.herokuapp.com/callback.php';
 //Initiate cURL.
 $ch = curl_init($url);
 //The JSON data.
 $jsonData = '{
     "recipient":{
-        "id":"'.$sender.'"
+        "id":"123456"
     },
     "message":{
     "attachment":{
@@ -60,7 +45,6 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//Execute the request
-if(!empty($input['entry'][0]['messaging'][0]['message'])){   
-    $result = curl_exec($ch);
-}
+//Execute the request 
+$result = curl_exec($ch);
+
