9bccae60bfad57747782eae72316becb334d6986
Umisagi <zenchiter@gmail.com>
2017-04-07 17:09:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -99,6 +99,9 @@ curl_setopt($statech, CURLOPT_POSTFIELDS, $jsonstateEncoded);
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($statech, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+curl_setopt($statech, CURLOPT_RETURNTRANSFER, true);
+
 //error_log("statedata : ".print_r($jsonstateEncoded,true));
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){   
