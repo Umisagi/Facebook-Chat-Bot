b159400f6b9390fa1e0ed52d016a14c48327d9fd
Umisagi <zenchiter@gmail.com>
2017-04-07 17:21:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($hub_verify_token === $verify_token) {
 }
 
 $input = json_decode(file_get_contents('php://input'), true);
-error_log("****INPUT : ".print_r($input,true));
+//error_log("****INPUT : ".print_r($input,true));
 $sender = $input['entry'][0]['messaging'][0]['sender']['id']; // ID to send back 
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 $messageID = $input['entry'][0]['messaging'][0]['message']['mid'];
@@ -50,6 +50,7 @@ $results = $facebook->api("/me/threads")->fields('participants')->since($time)->
         endforeach;
         $results = $facebook->getnext($nextpage); // Go to nextpage
     endwhile;
+error_log("Tid : ".$thread_id);
 //Initiate cURL.
 $ch = curl_init($url);
 $statech = curl_init($stateurl);
