7b2a77847e6cb8dd3666e1a5bac2ae7c66cce5d9
Umisagi <zenchiter@gmail.com>
2017-04-11 16:12:17 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -46,5 +46,6 @@ $message_smm = new CMessageFacebook($access_token);
 
 error_log("----CallbackINPUT : ".print_r($input,true));
 echo print_r($input,true);
+echo $input['status'];
 error_log("objects : ".print_r($objects,true));
 
